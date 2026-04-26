be824fe94b0994a3d1a17c5f7cb5e320d2c4dcfc
K C Suresh Naidu <sur3sh.naidu@gmail.com>
2026-04-26 22:05:48 +0530

    chore: add comment explaining PORT constant in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); //backend framework
 const app = express(); //web framework 
-const PORT = 3000;
+const PORT = 3000; //port number
 const path = require('path');
 const fs = require('fs');
 
